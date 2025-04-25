28da7a7f47c26028e5acd29f3ee4c9bda9be317f
Manuel Pereira <manilex2@gmail.com>
2025-04-25 10:54:10 -0500

    Arreglado bug de asunto en el correo que se enviaba

--- supabase/functions/notifications/index.ts
+++ supabase/functions/notifications/index.ts
@@ -649,7 +649,7 @@ app.get('/notifications/tramites', cors(corsOptions), async (req: express.Reques
             from: `${Deno.env.get("SENDGRID_SENDER_NAME")} <${Deno.env.get("SENDGRID_SENDER_EMAIL")}>`,
             to: userData.email, // Enviar directamente al usuario
             bcc: adminEmails.join(', '), // Enviar a los administradores en BCC
-            subject: `Trámite Vencido`,
+            subject: `Paso Vencido`,
             html: `
               <h2>PASO VENCIDO</h2>
               <p>El paso <strong>${tramite.tramite_name}</strong> asignado al usuario <strong>${userData.display_name}</strong> venció el <strong>${fechaFormateada}</strong>. Le recomendamos tomar las acciones pertinentes.</p>
